fb51a36b31727cfedd600f44ccbd42f26d4d8317
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-06-17 15:58:15 -0500

    Completed my player name tags, event tags, and score tags on html. Finished typing out the rules on the html page. Completed startGame button on javascript.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-document.querySelector("h1").innerHTML = "Hello World";
+let playerOneScore = document.getElementById("player_one_score");
+let playerTwoScore = document.getElementById("player_two_score");
+
+let btnStartGame = document.getElementById("btn_start_game");
+let btnRollDice = document.getElementById("btn_roll_dice");
+let btnRules = document.getElementById("btn_rules");
+
+let playerOneName = document.getElementById("player_one_name");
+let playerTwoName = document.getElementById("player_two_name");
+
+let playerOneEvents = document.getElementById("player_one_events");
+let playerTwoEvents = document.getElementById("player_two_events");
+
+function startNewGame() {
+    playerOneScore.innerHTML = 0;
+    playerTwoScore.innerHTML = 0;
+    playerOneName.innerHTML = prompt("Enter a name for player one...");
+    playerTwoName.innerHTML = prompt("Enter a name for player two...");
+    playerOneEvents.innerHTML = "The game is starting";
+    playerTwoEvents.innerHTML = "The game is starting";
+}
+
+function rollDice() {
+
+}
+
+function rules() {
+
+}
