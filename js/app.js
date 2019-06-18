74fdabd03efb93b9ba5e39fd1a006ba2b19cc6e4
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-06-18 14:44:24 -0500

    Added progress bar for each player and wrote js codes for the progress bars for each lap. Change styles of the progress bars.

--- js/app.js
+++ js/app.js
@@ -13,6 +13,13 @@ let playerTwoEvents = document.getElementById("player_two_events");
 let p1Score = playerOneScore.innerHTML = 0;
 let p2Score = playerTwoScore.innerHTML = 0;
 
+let p1FirstLapProgressBar = document.getElementById("p1_first_lap_progress_bar");
+let p1SecondLapProgressBar = document.getElementById("p1_second_lap_progress_bar");
+let p1ThirdLapProgressBar = document.getElementById("p1_third_lap_progress_bar");
+let p2FirstLapProgressBar = document.getElementById("p2_first_lap_progress_bar");
+let p2SecondLapProgressBar = document.getElementById("p2_second_lap_progress_bar");
+let p2ThirdLapProgressBar = document.getElementById("p2_third_lap_progress_bar");
+
 function startNewGame() {
     playerOneName.innerHTML = prompt("Enter a name for player one...");
     playerTwoName.innerHTML = prompt("Enter a name for player two...");
@@ -38,12 +45,14 @@ function rollDice() {
             playerOneEvents.innerHTML = "Wow you got doubles!!! You got " + score + " points.";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1FirstLapProgressBar.style.width = p1Score + "%";
         }
         else {
             score = randomNumber1 + randomNumber2;
             playerOneEvents.innerHTML = "Nothing is special, you got " + score + " points.";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1FirstLapProgressBar.style.width = p1Score + "%";
         }
     }
     if (p2Score < 100) {
@@ -54,12 +63,14 @@ function rollDice() {
             playerTwoEvents.innerHTML = "Wow you got doubles!!! You got " + score + " points.";
             p2Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2FirstLapProgressBar.style.width = p2Score + "%";
         }
         else {
             score = randomNumber1 + randomNumber2;
             playerTwoEvents.innerHTML = "Nothing is special, you got " + score + " points.";
             p2Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2FirstLapProgressBar.style.width = p2Score + "%";
         }
     }
     if (p1Score < 200 && p1Score >= 100) {
@@ -69,12 +80,14 @@ function rollDice() {
             playerOneEvents.innerHTML = "Nothing is special, you got " + score + " points.";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1SecondLapProgressBar.style.width = p1Score -100 + "%";
         }
         else if (randomNumber1 === 3) {
             score = -3;
             playerOneEvents.innerHTML = "Your tire burst, you lose 3 points";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1SecondLapProgressBar.style.width = p1Score -100 + "%";
         }
         else if (randomNumber1 === 5) {
             score = 5;
@@ -83,12 +96,14 @@ function rollDice() {
             playerOneScore.innerHTML = p1Score;
             p2Score -= 5;
             playerTwoScore.innerHTML = p2Score;
+            p1SecondLapProgressBar.style.width = p1Score -100 + "%";
         }
         else if (randomNumber1 === 6) {
             score = randomNumber1 * 2;
             playerOneEvents.innerHTML = "Wow you rolled " + randomNumber1 + " and you got " + score + " points";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1SecondLapProgressBar.style.width = p1Score -100 + "%";
         }
     }
     if (p2Score < 200 && p2Score >= 100) {
@@ -98,12 +113,14 @@ function rollDice() {
             playerTwoEvents.innerHTML = "Nothing is special, you got " + score + " points.";
             p2Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2SecondLapProgressBar.style.width = p2Score -100 + "%";
         }
         else if (randomNumber1 === 3) {
             score = -3;
             playerTwoEvents.innerHTML = "Your tire burst, you lose 3 points";
             p2Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2SecondLapProgressBar.style.width = p2Score -100 + "%";
         }
         else if (randomNumber1 === 5) {
             score = 5;
@@ -112,12 +129,14 @@ function rollDice() {
             playerTwoScore.innerHTML = p2Score;
             p1Score -= 5;
             playerOneScore.innerHTML = p1Score;
+            p2SecondLapProgressBar.style.width = p2Score -100 + "%";
         }
         else if (randomNumber1 === 6) {
             score = randomNumber1 * 2;
             playerTwoEvents.innerHTML = "Wow you rolled " + randomNumber1 + " and you got " + score + " points";
             p2Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2SecondLapProgressBar.style.width = p2Score -100 + "%";
         }
     }
     if (p1Score > 200 && p1Score < 300) {
@@ -127,12 +146,14 @@ function rollDice() {
             playerOneEvents.innerHTML = "Nothing is special, your score is " + score + " points.";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1ThirdLapProgressBar.style.width = p1Score - 200 + "%";
         }
         if (randomNumber1 === 1 || randomNumber1 === 6) {
             score = randomNumber1 * 2;
             playerOneEvents.innerHTML = "Wow you got doubles!! Your score is " + score + " points.";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1ThirdLapProgressBar.style.width = p1Score - 200 + "%";
         }
         if (randomNumber1 === 5) {
             score = randomNumber1;
@@ -141,30 +162,35 @@ function rollDice() {
             playerOneScore.innerHTML = p1Score;
             p2Score -= -5;
             playerTwoScore.innerHTML = p2Score;
+            p1ThirdLapProgressBar.style.width = p1Score - 200 + "%";
         }
         if (randomNumber1 === 8) {
             score = -10;
             playerOneEvents.innerHTML = "You got into an accident and lost 10 points";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1ThirdLapProgressBar.style.width = p1Score - 200 + "%";
         }
         if (randomNumber1 === 9) {
             score = -5;
             playerOneEvents.innerHTML = "You ran into a town, TRAFFIC, lost 5 points";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1ThirdLapProgressBar.style.width = p1Score - 200 + "%";
         }
         if (randomNumber1 === 10) {
             score = randomNumber1 + 15;
             playerOneEvents.innerHTML = "You used turbo boost. You scored " + score + " points.";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1ThirdLapProgressBar.style.width = p1Score - 200 + "%";
         }
         if (randomNumber1 === 11) {
             score = -8;
             playerOneEvents.innerHTML = "One of your tire burst. You lost 8 points.";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1ThirdLapProgressBar.style.width = p1Score - 200 + "%";
         }
         if (randomNumber1 === 3) {
             let randomNumber2 = randomNumber(8);
@@ -172,6 +198,7 @@ function rollDice() {
             playerOneEvents.innerHTML = "You rolled an additional 8-sided die. You scored " + score + " points.";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1ThirdLapProgressBar.style.width = p1Score - 200 + "%";
         }
         if (randomNumber1 === 7) {
             let randomNumber2 = randomNumber(10);
@@ -179,6 +206,7 @@ function rollDice() {
             playerOneEvents.innerHTML = "You rolled an additional 10-sided die. You scored " + score + " points.";
             p1Score += score;
             playerOneScore.innerHTML = p1Score;
+            p1ThirdLapProgressBar.style.width = p1Score - 200 + "%";
         }
         if (randomNumber1 === 12) {
             let randomNumber2 = randomNumber(20);
@@ -186,6 +214,7 @@ function rollDice() {
             playerOneEvents.innerHTML = "You rolled an additional 20-sided die. You scored" + score + " points.";
             p1Score += score;
             playerOneScore.innerHTML + p1Score;
+            p1ThirdLapProgressBar.style.width = p1Score - 200 + "%";
         }
     }
     if (p2Score > 200 && p2Score < 300) {
@@ -195,12 +224,14 @@ function rollDice() {
             playerTwoEvents.innerHTML = "Nothing is special, your score is " + score + " points.";
             p2Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2ThirdLapProgressBar.style.width = p2Score - 200 + "%";
         }
         if (randomNumber1 === 1 || randomNumber1 === 6) {
             score = randomNumber1 * 2;
             playerTwoEvents.innerHTML = "Wow you got doubles!! Your score is " + score + " points.";
             p2Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2ThirdLapProgressBar.style.width = p2Score - 200 + "%";
         }
         if (randomNumber1 === 5) {
             score = randomNumber1;
@@ -209,30 +240,35 @@ function rollDice() {
             playerTwoScore.innerHTML = p2Score;
             p1Score -= -5;
             playerOneScore.innerHTML = p1Score;
+            p2ThirdLapProgressBar.style.width = p2Score - 200 + "%";
         }
         if (randomNumber1 === 8) {
             score = -10;
             playerTwoEvents.innerHTML = "You got into an accident and lost 10 points";
             p2Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2ThirdLapProgressBar.style.width = p2Score - 200 + "%";
         }
         if (randomNumber1 === 9) {
             score = -5;
             playerTwoEvents.innerHTML = "You ran into a town, TRAFFIC, lost 5 points";
             p2Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2ThirdLapProgressBar.style.width = p2Score - 200 + "%";
         }
         if (randomNumber1 === 10) {
             score = randomNumber1 + 15;
             playerTwoEvents.innerHTML = "You used turbo boost. You scored " + score + " points.";
             p2Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2ThirdLapProgressBar.style.width = p2Score - 200 + "%";
         }
         if (randomNumber1 === 11) {
             score = -8;
             playerTwoEvents.innerHTML = "One of your tire burst. You lost 8 points.";
             p2Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2ThirdLapProgressBar.style.width = p2Score - 200 + "%";
         }
         if (randomNumber1 === 3) {
             let randomNumber2 = randomNumber(8);
@@ -240,6 +276,7 @@ function rollDice() {
             playerTwoEvents.innerHTML = "You rolled an additional 8-sided die. You scored " + score + " points.";
             p2Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2ThirdLapProgressBar.style.width = p2Score - 200 + "%";
         }
         if (randomNumber1 === 7) {
             let randomNumber2 = randomNumber(10);
@@ -247,13 +284,15 @@ function rollDice() {
             playerTwoEvents.innerHTML = "You rolled an additional 10-sided die. You scored " + score + " points.";
             p1Score += score;
             playerTwoScore.innerHTML = p2Score;
+            p2ThirdLapProgressBar.style.width = p2Score - 200 + "%";
         }
         if (randomNumber1 === 12) {
             let randomNumber2 = randomNumber(20);
             score = randomNumber1 + randomNumber2;
-            playerTwoEvents.innerHTML = "You rolled an additional 20-sided die. You scored" + score + " points.";
+            playerTwoEvents.innerHTML = "You rolled an additional 20-sided die. You scored " + score + " points.";
             p2Score += score;
             playerTwoScore.innerHTML + p2Score;
+            p2ThirdLapProgressBar.style.width = p2Score - 200 + "%";
         } 
     }
     if (p1Score >=300) {
