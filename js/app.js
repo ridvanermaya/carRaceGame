0eba61772a029ae8424d67b83f236f4198fffe37
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-06-18 10:29:06 -0500

    Finished the rollDice function. Now players can win the game by reaching 300 points. Finished the player win alert.

--- js/app.js
+++ js/app.js
@@ -119,4 +119,146 @@ function rollDice() {
             playerTwoScore.innerHTML = p2Score;
         }
     }
+    if (p1Score > 200 && p1Score < 300) {
+        let randomNumber1 = randomNumber(12);
+        if (randomNumber1 === 2 || randomNumber1 === 4) {
+            score = randomNumber1;
+            playerOneEvents.innerHTML = "Nothing is special, your score is " + score + " points.";
+            p1Score += score;
+            playerOneScore.innerHTML = p1Score;
+        }
+        if (randomNumber1 === 1 || randomNumber1 === 6) {
+            score = randomNumber1 * 2;
+            playerOneEvents.innerHTML = "Wow you got doubles!! Your score is " + score + " points.";
+            p1Score += score;
+            playerOneScore.innerHTML = p1Score;
+        }
+        if (randomNumber1 === 5) {
+            score = randomNumber1;
+            playerOneEvents.innerHTML = "You spilled oil on the road, the other player loses 5 points. Your score is " + randomNumber1 + " points.";
+            p1Score += score;
+            playerOneScore.innerHTML = p1Score;
+            p2Score -= -5;
+            playerTwoScore.innerHTML = p2Score;
+        }
+        if (randomNumber1 === 8) {
+            score = -10;
+            playerOneEvents.innerHTML = "You got into an accident and lost 10 points";
+            p1Score += score;
+            playerOneScore.innerHTML = p1Score;
+        }
+        if (randomNumber1 === 9) {
+            score = -5;
+            playerOneEvents.innerHTML = "You ran into a town, TRAFFIC, lost 5 points";
+            p1Score += score;
+            playerOneScore.innerHTML = p1Score;
+        }
+        if (randomNumber1 === 10) {
+            score = randomNumber1 + 15;
+            playerOneEvents.innerHTML = "You used turbo boost. You scored " + score + " points.";
+            p1Score += score;
+            playerOneScore.innerHTML = p1Score;
+        }
+        if (randomNumber1 === 11) {
+            score = -8;
+            playerOneEvents.innerHTML = "One of your tire burst. You lost 8 points.";
+            p1Score += score;
+            playerOneScore.innerHTML = p1Score;
+        }
+        if (randomNumber1 === 3) {
+            let randomNumber2 = randomNumber(8);
+            score = randomNumber1 + randomNumber2;
+            playerOneEvents.innerHTML = "You rolled an additional 8-sided die. You scored " + score + " points.";
+            p1Score += score;
+            playerOneScore.innerHTML = p1Score;
+        }
+        if (randomNumber1 === 7) {
+            let randomNumber2 = randomNumber(10);
+            score = randomNumber1 + randomNumber2;
+            playerOneEvents.innerHTML = "You rolled an additional 10-sided die. You scored " + score + " points.";
+            p1Score += score;
+            playerOneScore.innerHTML = p1Score;
+        }
+        if (randomNumber1 === 12) {
+            let randomNumber2 = randomNumber(20);
+            score = randomNumber1 + randomNumber2;
+            playerOneEvents.innerHTML = "You rolled an additional 20-sided die. You scored" + score + " points.";
+            p1Score += score;
+            playerOneScore.innerHTML + p1Score;
+        }
+    }
+    if (p2Score > 200 && p2Score < 300) {
+        let randomNumber1 = randomNumber(12);
+        if (randomNumber1 === 2 || randomNumber1 === 4) {
+            score = randomNumber1;
+            playerTwoEvents.innerHTML = "Nothing is special, your score is " + score + " points.";
+            p2Score += score;
+            playerTwoScore.innerHTML = p2Score;
+        }
+        if (randomNumber1 === 1 || randomNumber1 === 6) {
+            score = randomNumber1 * 2;
+            playerTwoEvents.innerHTML = "Wow you got doubles!! Your score is " + score + " points.";
+            p2Score += score;
+            playerTwoScore.innerHTML = p2Score;
+        }
+        if (randomNumber1 === 5) {
+            score = randomNumber1;
+            playerTwoEvents.innerHTML = "You spilled oil on the road, the other player loses 5 points. Your score is " + randomNumber1 + " points.";
+            p2Score += score;
+            playerTwoScore.innerHTML = p2Score;
+            p1Score -= -5;
+            playerOneScore.innerHTML = p1Score;
+        }
+        if (randomNumber1 === 8) {
+            score = -10;
+            playerTwoEvents.innerHTML = "You got into an accident and lost 10 points";
+            p2Score += score;
+            playerTwoScore.innerHTML = p2Score;
+        }
+        if (randomNumber1 === 9) {
+            score = -5;
+            playerTwoEvents.innerHTML = "You ran into a town, TRAFFIC, lost 5 points";
+            p2Score += score;
+            playerTwoScore.innerHTML = p2Score;
+        }
+        if (randomNumber1 === 10) {
+            score = randomNumber1 + 15;
+            playerTwoEvents.innerHTML = "You used turbo boost. You scored " + score + " points.";
+            p2Score += score;
+            playerTwoScore.innerHTML = p2Score;
+        }
+        if (randomNumber1 === 11) {
+            score = -8;
+            playerTwoEvents.innerHTML = "One of your tire burst. You lost 8 points.";
+            p2Score += score;
+            playerTwoScore.innerHTML = p2Score;
+        }
+        if (randomNumber1 === 3) {
+            let randomNumber2 = randomNumber(8);
+            score = randomNumber1 + randomNumber2;
+            playerTwoEvents.innerHTML = "You rolled an additional 8-sided die. You scored " + score + " points.";
+            p2Score += score;
+            playerTwoScore.innerHTML = p2Score;
+        }
+        if (randomNumber1 === 7) {
+            let randomNumber2 = randomNumber(10);
+            score = randomNumber1 + randomNumber2;
+            playerTwoEvents.innerHTML = "You rolled an additional 10-sided die. You scored " + score + " points.";
+            p1Score += score;
+            playerTwoScore.innerHTML = p2Score;
+        }
+        if (randomNumber1 === 12) {
+            let randomNumber2 = randomNumber(20);
+            score = randomNumber1 + randomNumber2;
+            playerTwoEvents.innerHTML = "You rolled an additional 20-sided die. You scored" + score + " points.";
+            p2Score += score;
+            playerTwoScore.innerHTML + p2Score;
+        } 
+    }
+    if (p1Score >=300) {
+        alert(playerOneName.innerHTML + " wins!");
+    }
+    if (p2Score >=300) {
+        alert(playerTwoName.innerHTML + " wins!");
+    }
 }
